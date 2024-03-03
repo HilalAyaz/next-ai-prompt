bd3011ccfca1bb112c03ae1084ff988874d24c41
Hilal Ayaz <134634880+HilalAyaz@users.noreply.github.com>
2024-03-03 09:41:13 +0500

    .

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,37 +1,53 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
-
 import Form from "@components/Form";
+
+const UpdatePromptPage = () => {
+ 
+  return (
+    <Suspense fallback={<><p>Loading...</p></>}>
+      <UpdatePrompt />
+    </Suspense>
+  );
+};
+
+export default UpdatePromptPage;
+
 
 const UpdatePrompt = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const promptId = searchParams.get("id");
-
-  const [post, setPost] = useState({ prompt: "", tag: "", });
-  const [submitting, setIsSubmitting] = useState(false);
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
 
   useEffect(() => {
     const getPromptDetails = async () => {
       const response = await fetch(`/api/prompt/${promptId}`);
       const data = await response.json();
-
       setPost({
         prompt: data.prompt,
         tag: data.tag,
       });
     };
 
-    if (promptId) getPromptDetails();
+    if (promptId) {
+      getPromptDetails();
+    }
   }, [promptId]);
 
-  const updatePrompt = async (e) => {
-    e.preventDefault();
-    setIsSubmitting(true);
+  const updatePrompt = async (event) => {
+    event.preventDefault();
+    setSubmitting(true);
 
-    if (!promptId) return alert("Missing PromptId!");
+    if (!promptId) {
+      return alert("Prompt ID not found.");
+    }
 
     try {
       const response = await fetch(`/api/prompt/${promptId}`, {
@@ -48,13 +64,13 @@ const UpdatePrompt = () => {
     } catch (error) {
       console.log(error);
     } finally {
-      setIsSubmitting(false);
+      setSubmitting(false);
     }
   };
 
   return (
     <Form
-      type='Edit'
+      type="Edit"
       post={post}
       setPost={setPost}
       submitting={submitting}
@@ -62,5 +78,3 @@ const UpdatePrompt = () => {
     />
   );
 };
-
-export default UpdatePrompt;
