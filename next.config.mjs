bed26025f9e60ada779d5ee90e9badbcd011d3cf
Hilal Ayaz <134634880+HilalAyaz@users.noreply.github.com>
2024-03-08 09:06:09 +0500

    .

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,14 @@ const nextConfig = {
     serverComponentsExternalPackages: ['mongoose']
   },
   images: {
-    domains: ['lh3.googleusercontent.com']
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**'
+      }
+    ]
   },
   webpack (config) {
     config.experiments = {
